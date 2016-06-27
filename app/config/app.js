ba5946cb09256fd40132356ea26da63c82b297f4
Vincent <bit23>
2016-06-27 19:01:30 +0200

    init angular-aston

---
+++ app/config/app.js
@@ -0,0 +1,25 @@
+var app = angular.module("MonApplication", 
+	[
+	"ngRoute", 
+	"ngMessages"
+	]
+);
+
+app.config(function($routeProvider) {
+	/*$routeProvider.when('/', {
+		templateUrl : 'app/views/accueil.html',
+		controller : 'accueilController'
+	})*/
+	$routeProvider.when('/form', {
+		templateUrl : 'app/views/formulaire.html',
+		controller : 'formController'
+	})
+	.when('/auteurs', {
+		templateUrl : 'app/views/auteurs.html',
+		controller : 'auteursController'
+	})
+	.when('/auteur/:id_auteur', {
+		templateUrl : 'app/views/auteur.html',
+		controller : 'auteurController'
+	});
+})
